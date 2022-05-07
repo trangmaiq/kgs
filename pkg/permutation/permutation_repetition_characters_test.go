3440c25657d89a4fe641cea2253df4df8836ea5e
Trang Mai <trangmaiq@gmail.com>
2022-05-07 09:54:36 +0700

    permutation using channel

--- pkg/permutation/permutation_repetition_characters_test.go
+++ pkg/permutation/permutation_repetition_characters_test.go
@@ -2,12 +2,33 @@ package permutation
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestPermutation(t *testing.T) {
 	result := Permutation([]string{}, "", []string{"A", "B", "C"}, 2)
 	expected := []string{"AA", "AB", "AC", "BA", "BB", "BC", "CA", "CB", "CC"}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Fail()
+	}
+}
+
+func TestPermutationChan(t *testing.T) {
+	var (
+		result   []string
+		expected = []string{"AA", "AB", "AC", "BA", "BB", "BC", "CA", "CB", "CC"}
+	)
+
+	out := PermutationChan([]string{"A", "B", "C"}, 2)
+	for o := range out {
+		result = append(result, o)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[j] > result[i]
+	})
 
 	if !reflect.DeepEqual(result, expected) {
 		t.Fail()
