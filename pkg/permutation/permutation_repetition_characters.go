ad18c8cca75abdb8a076d6ef4fe19d70c0b8ab90
Trang <40815729+trangmaiq@users.noreply.github.com>
2022-05-07 11:25:03 +0700

    permutation using channel (#2)

--- pkg/permutation/permutation_repetition_characters.go
+++ pkg/permutation/permutation_repetition_characters.go
@@ -1,5 +1,53 @@
 package permutation
 
+import (
+	"math"
+	"sync"
+)
+
+func output(data []string, out chan<- string, wg *sync.WaitGroup) {
+	for _, d := range data {
+		out <- d
+		wg.Done()
+	}
+}
+
+func permutationChan(wg *sync.WaitGroup, out chan string, data string, std []string, n int) {
+	var result []string
+	for _, s := range std {
+		var d = data
+		d += s
+
+		if len(d) == n {
+			result = append(result, d)
+		} else {
+			permutationChan(wg, out, d, std, n)
+		}
+	}
+
+	go output(result, out, wg)
+}
+
+func PermutationChan(std []string, n int) <-chan string {
+	var (
+		out   = make(chan string)
+		wg    sync.WaitGroup
+		count = int(math.Pow(float64(len(std)), float64(n)))
+	)
+
+	wg.Add(count)
+	go func() {
+		permutationChan(&wg, out, "", std, n)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
 func Permutation(result []string, data string, std []string, n int) []string {
 	for _, s := range std {
 		var d = data
