bb00c2d5c05c7bec745537be7897371d684192cd
Trang <40815729+trangmaiq@users.noreply.github.com>
2022-05-15 15:09:27 +0700

    add persister (#7)

--- cmd/serve/root.go
+++ cmd/serve/root.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"github.com/trangmaiq/kgs/registry"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func NewServeCmd() *cobra.Command {
@@ -16,9 +18,16 @@ func NewServeCmd() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			println("serving...")
 
+			dsn := "<user>:<password>@tcp(127.0.0.1:3306)/keys?charset=utf8mb4&parseTime=True&loc=Local"
+			db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+			if err != nil {
+				log.Println("unable to initialize db connection")
+				os.Exit(1)
+			}
+
 			routes := gin.Default()
 
-			_, err := registry.New(routes)
+			_, err = registry.New(routes, db)
 			if err != nil {
 				log.Println("unable to create new registry")
 				os.Exit(1)
--- registry/default_registry.go
+++ registry/default_registry.go
@@ -2,33 +2,37 @@ package registry
 
 import (
 	"context"
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 	"github.com/trangmaiq/kgs/internal/handler"
+	"github.com/trangmaiq/kgs/persistence/sql"
 )
 
 var _ Registry = new(DefaultRegistry)
 
 type DefaultRegistry struct {
-	engine *gin.Engine
+	engine      *gin.Engine
+	routerGroup *gin.RouterGroup
 
-	urlRouterGroup *gin.RouterGroup
+	persister *sql.Persister
 }
 
-func New(engine *gin.Engine) (Registry, error) {
+func New(engine *gin.Engine, db *gorm.DB) (Registry, error) {
 	var (
 		r   = new(DefaultRegistry)
-		err = r.Init(context.TODO(), engine)
+		err = r.Init(context.TODO(), engine, db)
 	)
 
 	return r, err
 }
 
-func (r *DefaultRegistry) Init(_ context.Context, engine *gin.Engine) error {
+func (r *DefaultRegistry) Init(_ context.Context, engine *gin.Engine, db *gorm.DB) error {
 	r.engine = engine
-	r.urlRouterGroup = engine.Group("/keys")
-
+	r.routerGroup = engine.Group("/keys")
 	r.Handler().RegisterRoutes()
+
+	r.persister = sql.NewPersister(db)
 
 	return nil
 }
@@ -38,10 +42,9 @@ func (r *DefaultRegistry) Handler() *handler.Handler {
 }
 
 func (r *DefaultRegistry) Routes() gin.IRoutes {
-	return r.engine
+	return r.routerGroup
 }
 
 func (r *DefaultRegistry) Persister() handler.Persister {
-	//TODO implement me
-	panic("implement me")
+	return r.persister
 }
